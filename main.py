b69b45583b471984a8a2e76a81f9125ce5fc7670
Samuel Barcik <samuel.barcik@gmail.com>
2024-02-04 18:50:23 +0100

    Uprava 1

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import mysql.connector
 
 connection = mysql.connector.connect(
     host = 'localhost',
+    port = '3306',
     user = 'root',
     password = 'root',
-    port = '3306',
     database = 'auta'
 )
 
 cursor = connection.cursor()
-sql_query = "SELECT * FROM slovenske_mesta"
+sql_query = "SELECT * FROM slovenske_mesta ORDER BY mesto"
 cursor.execute(sql_query)
 results = cursor.fetchall()
 
@@ -20,8 +20,25 @@ def sucet(a, b):
 
 print(sucet(91, 1))
 
+mesta = []
+id_cisla = []
+
 for row in results:
     print(row)
+    mesta.append(row[1])
+    id_cisla.append(row[0])
+
+print(mesta)
+print(id_cisla)
+
+def sucetIDCisiel(zoznam):
+    sucet = 0
+    for i in zoznam:
+        sucet += i
+
+    return sucet
+
+print(sucetIDCisiel(id_cisla))
 
 # Zatvorenie kurzora a pripojenia
 cursor.close()
