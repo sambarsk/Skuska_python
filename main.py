25f176cdd82c5db9eb7d48415b9a5f51d03babdc
Samuel Barcik <samuel.barcik@gmail.com>
2024-02-04 17:57:44 +0100

    Snazenie sa o pripojeni databazy

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host = "jdbc:mysql://localhost:3306",
+    user = "root",
+    password = "root",
+    database = "auta"
+)
+
+cursor = connection.cursor()
+sql_query = "SELECT * FROM slovenske_mesta"
+cursor.execute(sql_query)
+results = cursor.fetchall()
+
 def sucet(a, b):
 
     c = a + b
     return c
 
 print(sucet(91, 1))
+
+for row in results:
+    print(row)
+
+# Zatvorenie kurzora a pripojenia
+cursor.close()
+connection.close()
